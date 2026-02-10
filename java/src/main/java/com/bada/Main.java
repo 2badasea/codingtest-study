6eb88acf8debc98c28335e200f629b71fd12ff17
이바다 <gnjqtpf1@gmail.com>
2026-02-10 19:49:36 +0900

    작업

--- java/src/main/java/com/bada/Main.java
+++ java/src/main/java/com/bada/Main.java
@@ -14,12 +14,29 @@ public class Main {
 	public int[] solution(int[][] arr) {
 		int[] answer = {};
 		
-		
-		
-		
-		
-		
 		return answer;
+	}
+	
+	// 이진탐색 메서드 직접 구현해볼 것
+	public int binarySearch(int[] arr, int target) {
+		int start = 0;
+		int end = arr.length;		// end는 미포함
+		
+		// 조건문 지정
+		while (start < end) {		// '='를 할 경우 start == end == arr.length에 의해서 mid = arr.length가 될 수 있으며, ary[mid]는 예외가 터지게 된다.
+			int mid = (start + end) / 2;
+			int midVal = arr[mid];
+			
+			if (midVal == target) {
+				return mid;
+			} else if (midVal < target) {
+				start = mid + 1;
+			} else {
+				end = mid;
+			}
+		}
+		
+		return -1;
 	}
 	
 }
