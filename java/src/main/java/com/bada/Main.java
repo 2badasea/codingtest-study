e7d1d36ff85f1d49fa7d3b8fb2bf6c8213dd8625
이바다 <gnjqtpf1@gmail.com>
2026-02-12 14:31:56 +0900

    작업중

--- java/src/main/java/com/bada/Main.java
+++ java/src/main/java/com/bada/Main.java
@@ -1,42 +1,26 @@
 package com.bada;
 
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	
 	public static void main(String[] args) {
 		Main m = new Main();
 	}
 	
-	public int[] solution(int[][] arr) {
-		int[] answer = {};
-		
-		return answer;
+	public String solution(String s, int n) {
+		StringBuilder sb = new StringBuilder();
+		for (char c : s.toCharArray()) {
+			sb.append(processor(c, n));
+		}
+		return sb.toString();
 	}
 	
-	// 이진탐색 메서드 직접 구현해볼 것
-	public int binarySearch(int[] arr, int target) {
-		int start = 0;
-		int end = arr.length;		// end는 미포함
-		
-		// 조건문 지정
-		while (start < end) {		// '='를 할 경우 start == end == arr.length에 의해서 mid = arr.length가 될 수 있으며, ary[mid]는 예외가 터지게 된다.
-			int mid = (start + end) / 2;
-			int midVal = arr[mid];
-			
-			if (midVal == target) {
-				return mid;
-			} else if (midVal < target) {
-				start = mid + 1;
-			} else {
-				end = mid;
-			}
+	// n은 1이상, 25이하인 자연수 => 한 번에 대문자에서 소문자로 연달아 변경되는 경우는 없음
+	private char processor(char c, int n) {
+		if (Character.isWhitespace(c)) {
+			return c;
 		}
 		
-		return -1;
+		
 	}
 	
 }
